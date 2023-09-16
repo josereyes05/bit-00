bc36951225363b5170f216705163a6f3495d96a6
josereyes05 <hernandomontiel@hotmail.com>
2023-09-15 19:36:51 -0500

    ejercicio2.0

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,4 +1,11 @@
 let nombre = prompt('Hola, ingresa tu nombre')
 let apellido = prompt('Hola, ingresa tu apellido')
 alert(`Hola ${nombre} ${apellido}, Como 'tas? :)`)
-document.write(`Hola ${nombre} ${apellido}, Como tas? :)`)
+let confirmacion = confirm('Hola'+' '+nombre+' '+apellido+' '+'¿Deseas continuar?')
+
+//let con = confirmacion === true ? alert(`Bienvenido ${nombre} ${apellido}`) : alert(`Chao ${nombre} ${apellido}`)
+if (confirmacion === true) {
+    alert(`Bienvenido ${nombre} ${apellido}`)
+    document.write(`Hola ${nombre} ${apellido}, Como tas? :)`)
+}
+else alert(`Chao ${nombre} ${apellido}`)
