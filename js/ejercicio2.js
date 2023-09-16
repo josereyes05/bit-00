fc6cc40e1ac5b0aa8a9659582695e99bf2de7672
josereyes05 <hernandomontiel@hotmail.com>
2023-09-15 19:02:50 -0500

    Ejercicio 2 ;)

---
+++ js/ejercicio2.js
@@ -0,0 +1,4 @@
+let nombre = prompt('Hola, ingresa tu nombre')
+let apellido = prompt('Hola, ingresa tu apellido')
+alert(`Hola ${nombre} ${apellido}, Como 'tas? :)`)
+document.write(`Hola ${nombre} ${apellido}, Como tas? :)`)
