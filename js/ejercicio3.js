c9932378cc7c1c5088f3ab9fa65301bcc3ad34c6
josereyes05 <hernandomontiel@hotmail.com>
2023-09-20 19:31:39 -0500

    ejercicio api :)

--- js/ejercicio3.js
+++
@@ -1,8 +0,0 @@
-let nombre = prompt('Hola, ingresa tu nombre')
-
-let edad = prompt(`Hola, ${nombre} ¿Cuantos años tienes?`)
-
-let res = parseInt(edad)
-
-if(res >= 18) alert(`Hola ${nombre} eres mayor de edad`)
-else alert(`uy ${nombre} eres menor de edad`)
--- js/index.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-
-let nombre = prompt('Hola, ingresa tu nombre')
-
-alert(`Hola ${nombre}, Como 'tas? :)`)
